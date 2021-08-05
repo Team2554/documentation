08e4d84826cb50a9809b266124f840edca0db2be
unknown <sagarreddypatil@gmail.com>
2021-08-04 23:56:31 -0400

    search style change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ module.exports = {
         blogRouteBasePath: "/blog",
         indexPages: false,
         language: "en",
-        style: undefined,
+        style: "none",
         lunr: {
           tokenizerSeparator: /[\s\-]+/,
         },
